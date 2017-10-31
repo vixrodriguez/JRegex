ac5137c5a9c830d793903d9fc65de99e43c71bc0
Victor Rodriguez <vix206@gmail.com>
2017-10-31 16:46:32 -0500

    Add files via upload

--- EnumConst.java
+++ EnumConst.java
@@ -1,12 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 /**
- *
- * @author victor
+ * Enumerator that contains all constants that are used for JRegex
+ * @author Víctor Rodríguez
  */
 enum EnumConst {
     MIN_PORT_NETWORK(0),
@@ -21,6 +30,4 @@ enum EnumConst {
     public int getValue() {
         return value;
     }
-    
-    
 }
